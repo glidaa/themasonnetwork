f0cc4d349f32ee5a208fb1f918a64f88dd695552
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-03-28 21:57:30 +0200

    finished the jokes route

--- server/controllers/jokes.js
+++ server/controllers/jokes.js
@@ -11,20 +11,23 @@ const getJoke = async (req, res, next) => {
         });
 
         const config = new Configuration({
+            organization: process.env.CHAT_GPT_ORG_ID,
             apiKey: process.env.CHAT_GPT_API_KEY,
         })
         
         const openai = new OpenAIApi(config);
 
-        const chatGPTResponse = await openai.createCompletion({
-            model:"gpt-3.5-turbo",
-            prompt: "Create a funny headline based on the news: \"bitcoin is flling \"",
+        const chatGPTResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": "Create a funny headline based on the news : " + response.data.articles[0].title}],
             max_tokens: 20,
             temperature: 0.7,
         })
-
-        res.status(200).json({ message: chatGPTResponse });
+        
+        
+        res.status(200).json({ message: chatGPTResponse.data.choices[0].message.content });
     } catch (error) {
+        
         res.status(500).json({ message: error });
     }
 }
